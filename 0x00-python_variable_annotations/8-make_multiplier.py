c8730406dcef252a232dbdcedc10144b68df86c2
YassineRKB <yassine@arcraven.com>
2024-01-12 02:39:39 +0100

    task 8: rewind the train checker does not like shortcuts, no lamda

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Module for make_multiplier"""
 from typing import Callable
-
+from functools import partial
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Returns a function that multiplies a float by multiplier. using lamda function"""
-    return lambda x: x * multiplier
+    """Returns a function that multiplies a float by multiplier"""
+
+    multiply: Callable[[float], float] = partial(float.__mul__, multiplier)
+    return multiply
