a9cc02d081b477b3e73bd0eedd8102ca78559cb2
YassineRKB <yassine@arcraven.com>
2024-01-12 02:34:07 +0100

    task 8: neat use of lamda func

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier. using lamda function"""
+    return lambda x: x * multiplier
