f0eb9b5d7a9ad83e5652153ed34224944928a560
YassineRKB <yassine@arcraven.com>
2024-01-12 02:51:27 +0100

    task 12: yup typescript ptsd

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for type checking"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a list of tuples containing elements and their length"""
+    zoomed_in: List = [item for item in lst for x in range(factor)]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+zoom_2x = zoom_array(array)
+zoom_3x = zoom_array(array, 3)
