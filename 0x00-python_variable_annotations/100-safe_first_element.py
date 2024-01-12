de9757842429a58d427500788364dd506a594104
YassineRKB <yassine@arcraven.com>
2024-01-12 02:46:15 +0100

    task 10: carefully poking checker

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for duck typing an iterable object"""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of a list"""
+    if not lst:
+        return None
+    return lst[0]
