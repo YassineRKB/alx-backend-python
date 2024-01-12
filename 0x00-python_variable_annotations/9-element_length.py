d873b404ca5fe54b47c9380f7cefa9adf0431fbf
YassineRKB <yassine@arcraven.com>
2024-01-12 02:44:42 +0100

    task 9: carefully poking checker

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for element length"""
+from typing import List, Sequence, Tuple, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples containing elements and their length"""
+    return [(i, len(i)) for i in lst]
