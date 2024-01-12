78e8eb8cebc9449d815fdbeb4dd9a3b299bda81c
YassineRKB <yassine@arcraven.com>
2024-01-12 02:31:15 +0100

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for type annotations"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple of complex type"""
+    result = (k, float(v**2))
+    return result
