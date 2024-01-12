531ddd8aa4d4674eb7249ee0ab2ce6cfcaec8f79
YassineRKB <yassine@arcraven.com>
2024-01-12 02:48:42 +0100

    task 11: typescript ptsd anyone?

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for type annotations"""
+from typing import Any, Mapping, Union, TypeVar
+
+
+T = TypeVar('T')
+Def = Union[T, None]
+Res = Union[Any, T]
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    """Returns the value safely"""
+    if key not in dct:
+        return default
+    return dct[key]
