7302a633becb765dd06ad30a3f820359fdab60b4
YassineRKB <yassine@arcraven.com>
2024-01-16 00:44:02 +0100

    task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""module files for task 3"""
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """returns a asyncio.Task"""
+    return asyncio.create_task(wait_random(max_delay))
