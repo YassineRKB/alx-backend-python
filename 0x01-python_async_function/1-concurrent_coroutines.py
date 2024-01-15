3c73df927674a49020c7c78b0af701532686900f
YassineRKB <yassine@arcraven.com>
2024-01-16 00:22:23 +0100

    mods to 1 and 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,5 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """async function that waits for a random delay between 0 and max_delay"""
-    jobslist = [wait_random(max_delay) for _ in range(n)]
-    jobs = asyncio.as_completed(jobslist)
-    return [await job for job in jobs]
+    jobs = [wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(jobs)]
