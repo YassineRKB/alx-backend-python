bc8005fddf5c9991efe07a57a382aabbc69e0718
YassineRKB <yassine@arcraven.com>
2024-01-16 00:29:17 +0100

    mods to task 1 & task 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """module files for task 1"""
 import asyncio
+import random
 from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """async function that waits for a random delay between 0 and max_delay"""
-    jobslist = [wait_random(max_delay) for _ in range(n)]
-    jobs = asyncio.as_completed(jobslist)
-    return [await job for job in jobs]
+    """returns the list of all the delays"""
+    dlist = []
+    for i in range(n):
+        dlist.append(asyncio.create_task(wait_random(max_delay)))
+    return sorted(await asyncio.gather(*dlist))
