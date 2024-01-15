f5c99436b4226456098d93a3d97c5e1af79c004f
YassineRKB <yassine@arcraven.com>
2024-01-15 23:54:13 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""module files for task 1"""
+import asyncio
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """async function that waits for a random delay between 0 and max_delay"""
+    jobslist = [wait_random(max_delay) for _ in range(n)]
+    jobs = asyncio.as_completed(jobslist)
+    return [await job for job in jobs]
