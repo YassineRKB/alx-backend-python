638b14863634dbf0cc7722d01db3ecad7aaa1e00
YassineRKB <yassine@arcraven.com>
2024-01-16 20:31:37 +0100

    task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""module for task 2"""
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measures the total runtime and returns it"""
+    start = time.perf_counter()
+    corouts = []
+    for i in range(4):
+        corouts.append(async_comprehension())
+    await asyncio.gather(*corouts)
+    elapsed = time.perf_counter() - start
+    return elapsed
